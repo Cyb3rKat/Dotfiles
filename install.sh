00bf8a88e99743e2e42b09e0feb7e32d50a9b9ba
Alisa <khodadadi.alisa@gmail.com>
2022-12-16 02:27:56 +0330

    installing tmux config

--- install.sh
+++ install.sh
@@ -7,3 +7,14 @@
 
 ## copying all lua files to the .config directory
 cp -R "$HOME/dotfiles/nvim/" "$HOME/.config/nvim/"
+
+
+
+
+########
+# tmux #
+########
+#install plugin manager for tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -sf "~/dotfiles/tmux/.tmux.conf" "~/.tmux.conf"
+# -->>>> to install the tmux plugins on first load press ctr + a then press SHIFT + I 
